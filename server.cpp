43b50873d8520004f4ba2b4adef8f35da8ccba5f
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2020-07-21 20:32:37 +0600

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -6,6 +6,90 @@
 
 using namespace std;
 
+void write_file(int sock, const char* filename) {
+	int n;
+	FILE* fp;
+
+	char buffer[1024];
+
+	cout << "started file transfer";
+
+	fopen_s(&fp, filename, "wb");
+	while (1) {
+		recv(sock, buffer, 1024, 0);
+	
+		if (string(buffer) == "sentt cOmpl3t3") {
+		
+			break;
+			
+		}
+		fprintf(fp, "%s", buffer);
+		ZeroMemory(buffer, 1024);
+	}
+	fclose(fp);
+	return;
+}
+
+bool readdata(SOCKET sock, void* buf, int buflen)
+{
+	char* pbuf = (char*)buf;
+
+	while (buflen > 0)
+	{
+		int num = recv(sock, pbuf, buflen, 0);
+		if (num == SOCKET_ERROR)
+		{
+			if (WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				// optional: use select() to check for timeout to fail the read
+				continue;
+			}
+			return false;
+		}
+		else if (num == 0)
+			return false;
+
+		pbuf += num;
+		buflen -= num;
+	}
+
+	return true;
+}
+
+bool readlong(SOCKET sock, long* value)
+{
+	if (!readdata(sock, value, sizeof(value)))
+		return false;
+	*value = ntohl(*value);
+	return true;
+}
+
+bool readfile(SOCKET sock, FILE* f)
+{
+	long filesize;
+	if (!readlong(sock, &filesize))
+		return false;
+	if (filesize > 0)
+	{
+		char buffer[1024];
+		do
+		{
+			int num = min(filesize, sizeof(buffer));
+			if (!readdata(sock, buffer, num))
+				return false;
+			int offset = 0;
+			do
+			{
+				size_t written = fwrite(&buffer[offset], 1, num - offset, f);
+				if (written < 1)
+					return false;
+				offset += written;
+			} while (offset < num);
+			filesize -= num;
+		} while (filesize > 0);
+	}
+	return true;
+}
 
 int main()
 {
@@ -91,7 +175,31 @@ int main()
 
 			send(clientSocket, buf, 100, 0);
 
+			string cmd = string(buf);
 
+			if (cmd.find("download ", 0) == 0) {
+				cout << "In Download function \n";
+
+				cmd.erase(cmd.find("download "), 9);
+				
+				recv(clientSocket, buf2, 4, 0);
+				ZeroMemory(buf2, 4);
+				send(clientSocket, cmd.c_str(), 1024, 0);
+
+				FILE* filehandle;
+				fopen_s(&filehandle, cmd.c_str(), "wb");
+				if (filehandle != NULL)
+				{
+					bool ok = readfile(clientSocket , filehandle);
+					fclose(filehandle);
+
+				
+				}
+
+				/*write_file(clientSocket, cmd.c_str());*/
+			}
+
+			else{
 			char buffer[8];
 			recv(clientSocket, buffer, 8, 0);
 			send(clientSocket, "K", 8, 0);
@@ -114,7 +222,7 @@ int main()
 			
 			cout << buf2;
 
-
+			}
 
 
 
